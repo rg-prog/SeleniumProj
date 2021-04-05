b72438138b5ad85bf8a63e6de985cb6deabbe55d
gkgaj <gkgaj@192.168.0.17>
2021-04-04 23:28:03 -0400

    Acount summary updated

--- Mavenproject/src/main/java/com/webappsecurity/zero/Mavenproject/AccountSummary.java
+++ Mavenproject/src/main/java/com/webappsecurity/zero/Mavenproject/AccountSummary.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AccountSummary {
-	
+	//Update and save
 	@FindBy(linkText="Transfer Funds")
 	private WebElement transferFunds;
 	
